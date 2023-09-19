39ad027c82d20b8a73f4177c339939afcdd4977b
Nick Neyland <nickneyland@users.noreply.github.com>
2023-09-19 12:20:42 -0400

    initial file upload

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router"
+
+function BlogPosts() {
+    const router = useRouter()
+
+    return <div>
+        <h1>Blog Posts</h1>
+    </div>
+}
+
+export default BlogPosts
